362ba7330e232d2fcbe25601e0005d7ed4687531
Alex Sevilla <alex.sevilla@gmail.com>
2023-10-23 23:51:50 -0300

    new view for kosmo

---
+++ public/views/kosmo.js
@@ -0,0 +1,11 @@
+/* 
+   Kosmo V0.1 
+   Oct 23nd 2023
+*//*
+svgImage('kosmo', 'magenta', 10, "/img/kosmo.jpg", [0,18], 'lines');
+svgImage('kosmo', 'yellow', 10, "/img/kosmo.jpg", [0,16], 'lines');
+svgImage('kosmo', 'cyan', 10, "/img/kosmo.jpg", [0,20], 'lines');
+svgImage('bg', 'green', 10, "/img/kosmobg.jpg", [0,20], 'balls');*/
+svgImage('kosmo', 'red', 21, "/img/kosmo_p.jpg", [0,18], 'balls');
+svgImage('kosmo', 'blue', 21, "/img/kosmo_p.jpg", [0,8], 'balls');
+svgImage('bg', 'black', 21, "/img/kosmo_pbg.jpg", [0,10], 'lines');
